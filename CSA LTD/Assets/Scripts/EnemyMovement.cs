98b5b575eed82e4fa44a632ebe7042e6c569e58a
Anthony Uitz <aru5mf@virginia.edu>
2015-09-10 10:40:19 -0400

    added enemy spawn with movement towards end zone

---
+++ CSA LTD/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(0, 0, -1));
+    }
+}
---
+++ CSA LTD/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.X))
+        {
+            Vector3 position = new Vector3(Random.Range(-4.0F, 4.0F), 0.5F, Random.Range(15.5F, 18.5F));
+            Instantiate(obj, position, Quaternion.identity);   
+        }
+	}
+}
