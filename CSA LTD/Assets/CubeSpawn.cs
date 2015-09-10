deb1fc2b52f4bac78e4d277727e29017a751096a
Kevin Lin <kyl8wm@virginia.edu>
2015-09-09 23:57:49 -0400

    Test Cube Spawn

---
+++ CSA LTD/Assets/CubeSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour {
+	
+	public Vector3 ObjectSpawnPosition;
+	public GameObject obj;
+	
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.X)) {
+			Instantiate(obj, ObjectSpawnPosition, Quaternion.identity);
+		}
+	}
+}
