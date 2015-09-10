efd4c68ee6c17eaf6062d1481ce05444341329d5
Anthony Uitz <aru5mf@virginia.edu>
2015-09-10 16:46:17 -0400

    added towers on click

--- CSA LTD/Assets/Scripts/TileManager.cs
+++ CSA LTD/Assets/Scripts/TileManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TileManager : MonoBehaviour {
 
     Color initialColor;
+    public GameObject tower;
 
 	// Use this for initialization
 	void Start () {
@@ -11,10 +12,30 @@ public class TileManager : MonoBehaviour {
         initialColor = rend.material.color;
 	}
 	
-	void OnMouseOver()
+    void OnMouseOver()
     {
         Renderer rend = GetComponent<Renderer>();
-        rend.material.color = Color.red;
+        if (!rend.material.color.Equals(Color.red))
+        {
+            rend.material.color = Color.green;
+        }
+    }
+
+	void OnMouseDown()
+    {
+        Vector3 currPos = transform.position;
+
+        GameObject instance = Instantiate(tower, new Vector3(currPos.x + .5F, 1F, currPos.z - .5F), Quaternion.identity) as GameObject;
+
+        /* Renderer rend = GetComponent<Renderer>();
+        if(rend.material.color.Equals(initialColor) || rend.material.color.Equals(Color.green))
+        {
+            rend.material.color = Color.red;
+        }
+        else
+        {
+            rend.material.color = initialColor;
+        }*/
     }
 
     void OnMouseExit()
